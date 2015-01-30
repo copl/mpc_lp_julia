2090fe5404fa56225ee3858d37c7ad11f43753cc
Oliver Hinder <ohinder@gmail.com>
2015-01-29 16:41:51 -0800

    julia updated, c++ files started

---
+++ src/c++ code/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 28/01/2015
+ *      Author: Oliver
+ */
+#include <iostream>
+#include "interior_point_algorithm.h"
+
+int main()
+{
+  std::cout << "Hello World!";
+}
+
+
+
