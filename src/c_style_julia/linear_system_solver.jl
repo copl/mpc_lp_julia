0917d5067851b762a143d91b6f5c79203f61592c
Davood Shamsi <davood.shamsi@gmail.com>
2015-02-09 11:07:48 -0800

    Added few comments

--- src/c_style_julia/linear_system_solver.jl
+++ src/c_style_julia/linear_system_solver.jl
@@ -1,5 +1,18 @@
 # Functions
 # A,G, c, h, b, x, s, z, y, tau, kappa, q1, q2, q3, q4, q5, q6
+# Linear Equation Solver for steps (1) Affine direction (2) Corrector direction.
+# Input to the function
+#       problem_data::class_linear_program_input: Input data for the LP that includes A, G, h, b, c, m, n, k
+#       variables::class_linear_program_variables: Variables (primal and dual) of LP which includes x, y, s, z, tau, kappa
+#       rhs::class_linear_system_rhs: Right hand side for the Linear Equation. Right hand side is differnet for
+#               Affine direction and  Corrector direction
+#       K_newton_matrix::class_K_newton_matrix: This data structure is used to share information when we find
+#               Affine direction and Corrector direction. In the algorithm we first find Affine direction, and next Corrector direction.
+#               Since, the factorization calculated in  Affine direction can be used in finding Corrector direction,
+#               data is shared between these two function calls.
+#       
+# Output: The function modified LP variables in variables::class_linear_program_variables
+
 function solveLinearEquation(problem_data::class_linear_program_input,variables::class_linear_program_variables,rhs::class_linear_system_rhs,K_newton_matrix::class_K_newton_matrix)
 	A = problem_data.A
 	G = problem_data.G
